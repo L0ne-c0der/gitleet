b0e511a8370f43b1e2164ec3070cbf82e9796ca7
T Navadeep Vignesh <144514768+L0ne-c0der@users.noreply.github.com>
2025-08-22 19:19:19 +0530

    Time: 0 ms (100%), Space: 41.7 MB (42.6%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,30 +1,45 @@
-class Solution {
+ class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int x = 0;
-        int y = 0;
-        int dx = 1;
-        int dy = 0;
-        List<Integer> res = new ArrayList<>();
+        int n = matrix.length;
+        int m = matrix[0].length;
+        List<Integer> result = new ArrayList<>();
+        return spiralMatrix(result, matrix, 0, 0, n, m);
+    }
 
-        for (int i = 0; i < rows * cols; i++) {
-            res.add(matrix[y][x]);
-            matrix[y][x] = -101; // the range of numbers in matrix is from -100 to 100
-
-            if (!(0 <= x + dx && x + dx < cols && 0 <= y + dy && y + dy < rows) || matrix[y+dy][x+dx] == -101) {
-                //basically dx = -dy and dy = dx, whenever we go out of bounds
-                //or get duplicate (-101) positions
-                //dx is added to x, and dy added to y
-                int temp = dx;
-                dx = -dy;
-                dy = temp;
-            }
-
-            x += dx;
-            y += dy;
+    static List<Integer> spiralMatrix(List<Integer> arrayList, int[][] matrix, int startRow, int startCol, int endRow, int endCol) {
+        if (endCol <= startCol || endRow <= startRow) {
+            return arrayList;
         }
 
-        return res;        
+        //the single row or column condition is making duplicates
+        //so instead, check for the condition in the reverse traversing
+        //i.e, form right to left(single col) and from bottom to top(single row)
+
+        // Traverse top row from left to right
+        for (int j = startCol; j < endCol; j++) {
+            arrayList.add(matrix[startRow][j]);
+        }
+
+        // Traverse rightmost column from top to bottom
+        for (int i = startRow + 1; i < endRow; i++) {
+            arrayList.add(matrix[i][endCol - 1]);
+        }
+
+        // Traverse bottom row from right to left (only if there's more than 1 row)
+        if (endRow - startRow > 1) {
+            for (int j = endCol - 2; j >= startCol; j--) {
+                arrayList.add(matrix[endRow - 1][j]);
+            }
+        }
+
+        // Traverse leftmost column from bottom to top (only if there's more than 1 column)
+        if (endCol - startCol > 1) {
+            for (int i = endRow - 2; i > startRow; i--) {
+                arrayList.add(matrix[i][startCol]);
+            }
+        }
+
+        // Recursive call for the inner matrix
+        return spiralMatrix(arrayList, matrix, startRow + 1, startCol + 1, endRow - 1, endCol - 1);
     }
 }
