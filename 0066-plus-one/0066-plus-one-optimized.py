cbc751abf850328c5e8b5fd151e8993f90fb7f08
T Navadeep Vignesh <144514768+L0ne-c0der@users.noreply.github.com>
2025-08-25 19:57:33 +0530

    Time: 0 ms (100%), Space: 17.6 MB (79.24%) - LeetHub

--- 0066-plus-one/0066-plus-one-optimized.py
+++ 0066-plus-one/0066-plus-one-optimized.py
@@ -3,11 +3,10 @@ class Solution:
 
         for i in range(len(digits) - 1, -1, -1):
 
-            if digits[i] + 1 != 10:
+            if digits[i] < 9:
                 digits[i] += 1
                 return digits
             
             digits[i] = 0
 
-            if i == 0:
-                return [1] + digits
+        return [1] + digits
