37b4b687729a0820ac584284112c7a2cc5ce1a1c
T Navadeep Vignesh <144514768+L0ne-c0der@users.noreply.github.com>
2025-09-26 15:26:55 +0530

    Time: 359 ms (51.29%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -2,16 +2,11 @@
 #quality = AVG(rating/position) So, create a column rating/position ?
 #poor qual % = (no of things less than 3 / total ) * 100
 
-SELECT query_name , 
-    ROUND((AVG(rating/position)),2) AS quality,
-    ROUND(
-        (SELECT COUNT(rating) FROM Queries t2
-        WHERE 
-            t2.query_name = t1.query_name
-            AND t2.rating<3
-        ) * 100
-        / COUNT(rating)
-    ,2) AS poor_query_percentage
-FROM
-    Queries t1
-GROUP BY query_name;
+select
+query_name,
+round(avg(cast(rating as decimal) / position), 2) as quality,
+round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*), 2) as poor_query_percentage
+from
+queries
+group by
+query_name;
