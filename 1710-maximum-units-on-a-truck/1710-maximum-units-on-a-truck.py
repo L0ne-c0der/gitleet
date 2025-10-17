c5160ee003a0b48da789a5a77e50166e3d31627c
T Navadeep Vignesh <144514768+L0ne-c0der@users.noreply.github.com>
2025-10-17 12:17:00 +0530

    Time: 4 ms (80.17%), Space: 18.3 MB (10.69%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -4,24 +4,16 @@ class Solution:
         #we need to fill the boxes with most units_per_box
 
         boxTypes.sort(key= lambda boxes: -boxes[1])
-        boxes = 0
         units = 0
-        i=0
 
-        while i<len(boxTypes) and boxes<=truckSize:
-
-            [num_boxes, num_units] = boxTypes[i]
-
-            if boxes+num_boxes <= truckSize:
-                boxes+=num_boxes
-
-            else:
-                num_boxes = truckSize - boxes
-                boxes = truckSize
-                
-            i+=1
-            units += (num_boxes * num_units)
-
+        for boxes,uni in boxTypes:
+            
+            boxes = min(boxes, truckSize)
+            units+= uni * boxes
+            truckSize -= boxes
+            if truckSize == 0:
+                break
+        
         return units
 
 
