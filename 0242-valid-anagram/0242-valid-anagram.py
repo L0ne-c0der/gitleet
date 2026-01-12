f180cc29025d2c09ff751b5e7c59982bbea73962
T Navadeep Vignesh <144514768+L0ne-c0der@users.noreply.github.com>
2026-01-12 10:10:28 +0530

    Time: 3 ms (98.52%), Space: 19.5 MB (7.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,7 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        def str_to_dict(string) -> dict:
-            mapp = dict()
-            for char in string:
-                if char in mapp:
-                    mapp[char] += 1
-                    continue
-                mapp[char] = 1
-            return mapp
-    
-        return str_to_dict(s) == str_to_dict(t)
+        if len(s) != len(t):
+            return False
+        return Counter(s) == Counter(t)
+            
