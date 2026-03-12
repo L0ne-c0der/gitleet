09e3409fb2aedf08eee768085eb17bd705e36c07
T Navadeep Vignesh <144514768+L0ne-c0der@users.noreply.github.com>
2026-03-12 23:27:21 +0530

    Time: 3 ms (32.78%), Space: 19.2 MB (100%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.py
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.py
@@ -1,24 +1,40 @@
 class Solution:
     def canBeIncreasing(self, nums: List[int]) -> bool:
-        def is_sorted(arr) -> bool: 
-            ''' To check if the given array is increasing or not.'''
-            l = len(arr)
-            if l == 1:
-                return True    
-            for i in range(l-1):
-                if arr[i] >= arr[i+1]:
-                    return False
+        '''
+        We check for 2 drops.
+
+        When there is a first drop, 
+        we check if the number at 'pos+1' index 
+        is greater than 'pos-1'
+
+        1. If yes, then we drop the current number arr[pos], 
+        like dropping 3 in 1,3,2
+
+        2. If no, then we drop the next number, as it would be like [2, 3, 1], and hence we drop arr[pos+1], in this case, '1'
+
+        Then, we check for another drop. 
+
+        If 2 (or more) drops present, we will return False, else True
+        '''
+        if len(nums)<=2:
             return True
+        count = 0
+        i = 0
+        while i < len(nums)-1 and count<2:
+            if nums[i] >= nums[i+1]:  #if current >= next
+                if i==0:
+                    nums[i] = 0 # for cases where 2 pos = 0 pos
+                    #like 2, 1, 2
+                    
+                elif i>0 and (nums[i+1] > nums[i-1]): #if prev < next
+                    nums[i] = nums[i-1] #replace curr with prev 
+                else:
+                    nums[i+1] = nums[i] #replace next with curr
+                count+=1 #increase count
+            i+=1
 
-        #even in case the array is already in increasing order, 
-        #removing first element will still keep it the same
+        return False if count>=2 else True     
+                
+        
 
-        for i in range(len(nums)):
-            if is_sorted(nums[:i] + nums[i+1:]): 
-                # if array is increasing after removing 
-                #element in index i
-                return True
 
-        # even with all combination of elements 
-        #being removed, it is unsorted
-        return False
